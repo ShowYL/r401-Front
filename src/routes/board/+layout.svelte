993f96045c24b56e0f98826fd48888de44fecf44
Yann L <130548077+ShowYL@users.noreply.github.com>
2025-03-16 14:08:23 +0100

    Update navigation links, improve cookie handling, and add server-side JWT authentication

--- src/routes/board/+layout.svelte
+++ src/routes/board/+layout.svelte
@@ -12,32 +12,37 @@
                 <img src="/logo-white-version.png" alt="logo" class="w-3/4 h-10">
                 <ul class="list-none m-0 p-0 flex flex-col h-1/2">
                     <li class="flex items-center my-2 mx-4">
-                        <a href="accueil" class="nav-button flex items-center bg-gray-800 text-white p-2 rounded cursor-pointer no-underline h-1/2">
+                        <a href="/board/accueil" class="nav-button flex items-center bg-gray-800 text-white p-2 rounded cursor-pointer no-underline h-1/2"
+                        data-sveltekit-reload>
                             <div class="vertical-hr bg-gray-600 h-3/2 w-1 mr-3 hidden rounded"></div>
                             <img src="/accueil.png" alt="Accueil logo" class="w-1/4 h-5/6 mr-2">Accueil
                         </a>
                     </li>
                     <li class="flex items-center my-2 mx-4">
-                        <a href="joueur" class="nav-button flex items-center bg-gray-800 text-white p-2 rounded cursor-pointer no-underline h-1/2">
+                        <a href="/board/joueur" class="nav-button flex items-center bg-gray-800 text-white p-2 rounded cursor-pointer no-underline h-1/2"
+                        data-sveltekit-reload>
                             <div class="vertical-hr bg-gray-600 h-3/2 w-1 mr-3 hidden rounded"></div>
                             <img src="/joueur-de-rugby.png" alt="Joueur logo" class="w-1/4 h-5/6 mr-2">Joueurs
                         </a>
                     </li>
                     <li class="flex items-center my-2 mx-4">
-                        <a href="match" class="nav-button flex items-center bg-gray-800 text-white p-2 rounded cursor-pointer no-underline h-1/2">
+                        <a href="/board/match" class="nav-button flex items-center bg-gray-800 text-white p-2 rounded cursor-pointer no-underline h-1/2"
+                        data-sveltekit-reload>
                             <div class="vertical-hr bg-gray-600 h-3/2 w-1 mr-3 hidden rounded"></div>
                             <img src="/match.png" alt="Matchs logos" class="w-1/4 h-5/6 mr-2">Matchs
                         </a>
                     </li>
                     <li class="flex items-center my-2 mx-4">
-                        <a href="selection" class="nav-button flex items-center bg-gray-800 text-white p-2 rounded cursor-pointer no-underline h-1/2">
+                        <a href="/board/selection" class="nav-button flex items-center bg-gray-800 text-white p-2 rounded cursor-pointer no-underline h-1/2"
+                        data-sveltekit-reload>
                             <div class="vertical-hr bg-gray-600 h-3/2 w-1 mr-3 hidden rounded"></div>
                             <img src="/selection.png" alt="Selection logo" class="w-1/4 h-5/6 mr-2">Selection
                         </a>
                     </li>
                 </ul>
                 <div class="logout-div">
-                    <a id="logout-button" class="logout-button flex items-center bg-gray-700 text-white p-2 rounded cursor-pointer no-underline h-1/2 my-1 mx-4">
+                    <a id="logout-button" class="logout-button flex items-center bg-gray-700 text-white p-2 rounded cursor-pointer no-underline h-1/2 my-1 mx-4"
+                    data-sveltekit-reload>
                         <img src="/logout.png" alt="Log-Out logo" class="w-1/4 h-5/6 mr-2">Log-Out
                     </a>
                 </div>
