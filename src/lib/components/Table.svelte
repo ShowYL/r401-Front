438992d6fc0ca74cb77fd1268f6cc4bbacdc88e6
Yann L <130548077+ShowYL@users.noreply.github.com>
2025-03-20 08:35:43 +0100

    logout functionnality

--- src/lib/components/Table.svelte
+++ src/lib/components/Table.svelte
@@ -1,35 +1,41 @@
 <script>
-
 	let { data = null } = $props();
 
 	const headers = data && data.length > 0 ? Object.keys(data[0]) : [];
-    const body = () => {
-        let res = []
-        data.forEach(element => {
-            res.push(Object.values(element))
-        });
-        return res
-    }
+
+	const body = () => {
+		if (!data || data.length === 0) return [];
+
+		let res = [];
+		data.forEach((element) => {
+			res.push(Object.values(element));
+		});
+
+		return res
+	};
+
 
 </script>
 
 <div class="m-2">
-	<table class="table-auto border border-collapse">
+	<table class="table-auto border-2">
 		<thead>
 			<tr>
 				{#each headers as header}
-					<th class="border p-2">{header}</th>
+					<th class="cursor-pointer border p-2">
+						{header}
+					</th>
 				{/each}
 			</tr>
 		</thead>
 		<tbody>
-            {#each body() as row}
-                <tr>
-                    {#each row as donnee}
-                        <td class="border p-2">{donnee}</td>
-                    {/each}
-                </tr>
-            {/each}
-        </tbody>
+			{#each body() as row}
+				<tr>
+					{#each row as donnee}
+						<td class="border p-2">{donnee}</td>
+					{/each}
+				</tr>
+			{/each}
+		</tbody>
 	</table>
 </div>
--- src/routes/board/+layout.svelte
+++ src/routes/board/+layout.svelte
@@ -1,62 +1,93 @@
 <script>
-    import '../../app.css';
-    import { onMount } from 'svelte';
+	import '../../app.css';
+	import { onMount } from 'svelte';
+	import { eraseCookie } from '$lib/utils';
+	import { goto } from '$app/navigation';
 
-    let { children } = $props();
+	let { children } = $props();
 
+	function deconnexion() {
+		eraseCookie('token');
+		goto("/");
+	}
 </script>
-<div class="flex flex-row w-full h-screen"> <!-- container -->
-    <div class='w-48 bg-gray-300 shadow-md flex'>  <!-- left bar -->
-        <div class='bg-gray-800 text-white py-2 w-full flex justify-center'> <!-- nav -->
-            <nav class="flex flex-col items-center justify-between h-full">
-                <img src="/logo-white-version.png" alt="logo" class="w-3/4 h-10">
-                <ul class="list-none m-0 p-0 flex flex-col h-1/2">
-                    <li class="flex items-center my-2 mx-4">
-                        <a href="/board/accueil" class="nav-button flex items-center bg-gray-800 text-white p-2 rounded cursor-pointer no-underline h-1/2"
-                        data-sveltekit-reload>
-                            <div class="vertical-hr bg-gray-600 h-3/2 w-1 mr-3 hidden rounded"></div>
-                            <img src="/accueil.png" alt="Accueil logo" class="w-1/4 h-5/6 mr-2">Accueil
-                        </a>
-                    </li>
-                    <li class="flex items-center my-2 mx-4">
-                        <a href="/board/joueur" class="nav-button flex items-center bg-gray-800 text-white p-2 rounded cursor-pointer no-underline h-1/2"
-                        data-sveltekit-reload>
-                            <div class="vertical-hr bg-gray-600 h-3/2 w-1 mr-3 hidden rounded"></div>
-                            <img src="/joueur-de-rugby.png" alt="Joueur logo" class="w-1/4 h-5/6 mr-2">Joueurs
-                        </a>
-                    </li>
-                    <li class="flex items-center my-2 mx-4">
-                        <a href="/board/match" class="nav-button flex items-center bg-gray-800 text-white p-2 rounded cursor-pointer no-underline h-1/2"
-                        data-sveltekit-reload>
-                            <div class="vertical-hr bg-gray-600 h-3/2 w-1 mr-3 hidden rounded"></div>
-                            <img src="/match.png" alt="Matchs logos" class="w-1/4 h-5/6 mr-2">Matchs
-                        </a>
-                    </li>
-                    <li class="flex items-center my-2 mx-4">
-                        <a href="/board/selection" class="nav-button flex items-center bg-gray-800 text-white p-2 rounded cursor-pointer no-underline h-1/2"
-                        data-sveltekit-reload>
-                            <div class="vertical-hr bg-gray-600 h-3/2 w-1 mr-3 hidden rounded"></div>
-                            <img src="/selection.png" alt="Selection logo" class="w-1/4 h-5/6 mr-2">Selection
-                        </a>
-                    </li>
-                </ul>
-                <div class="logout-div">
-                    <a id="logout-button" class="logout-button flex items-center bg-gray-700 text-white p-2 rounded cursor-pointer no-underline h-1/2 my-1 mx-4"
-                    data-sveltekit-reload>
-                        <img src="/logout.png" alt="Log-Out logo" class="w-1/4 h-5/6 mr-2">Log-Out
-                    </a>
-                </div>
-            </nav>
-        </div>
-    </div>
-    <div class='flex-grow bg-gray-400 flex flex-col items-center'> <!-- rigth content-->
-        <div class='w-full h-24 -mt-5'> <!-- top bar-->
-            <h1>Admin</h1>
-        </div>
-        <div class='w-full mt-5 h-full'> <!-- main content-->
-            {@render children()}
-        </div>
-        <!-- footer after -->
-    </div>
-</div>
 
+<div class="flex h-screen w-full flex-row">
+	<!-- container -->
+	<div class="flex w-48 bg-gray-300 shadow-md">
+		<!-- left bar -->
+		<div class="flex w-full justify-center bg-gray-800 py-2 text-white">
+			<!-- nav -->
+			<nav class="flex h-full flex-col items-center justify-between">
+				<img src="/logo-white-version.png" alt="logo" class="h-10 w-3/4" />
+				<ul class="m-0 flex h-1/2 list-none flex-col p-0">
+					<li class="mx-4 my-2 flex items-center">
+						<a
+							href="/board/accueil"
+							class="nav-button flex h-1/2 cursor-pointer items-center rounded bg-gray-800 p-2 text-white no-underline"
+							data-sveltekit-reload
+						>
+							<div class="vertical-hr mr-3 hidden h-3/2 w-1 rounded bg-gray-600"></div>
+							<img src="/accueil.png" alt="Accueil logo" class="mr-2 h-5/6 w-1/4" />Accueil
+						</a>
+					</li>
+					<li class="mx-4 my-2 flex items-center">
+						<a
+							href="/board/joueur"
+							class="nav-button flex h-1/2 cursor-pointer items-center rounded bg-gray-800 p-2 text-white no-underline"
+							data-sveltekit-reload
+						>
+							<div class="vertical-hr mr-3 hidden h-3/2 w-1 rounded bg-gray-600"></div>
+							<img src="/joueur-de-rugby.png" alt="Joueur logo" class="mr-2 h-5/6 w-1/4" />Joueurs
+						</a>
+					</li>
+					<li class="mx-4 my-2 flex items-center">
+						<a
+							href="/board/match"
+							class="nav-button flex h-1/2 cursor-pointer items-center rounded bg-gray-800 p-2 text-white no-underline"
+							data-sveltekit-reload
+						>
+							<div class="vertical-hr mr-3 hidden h-3/2 w-1 rounded bg-gray-600"></div>
+							<img src="/match.png" alt="Matchs logos" class="mr-2 h-5/6 w-1/4" />Matchs
+						</a>
+					</li>
+					<li class="mx-4 my-2 flex items-center">
+						<a
+							href="/board/selection"
+							class="nav-button flex h-1/2 cursor-pointer items-center rounded bg-gray-800 p-2 text-white no-underline"
+							data-sveltekit-reload
+						>
+							<div class="vertical-hr mr-3 hidden h-3/2 w-1 rounded bg-gray-600"></div>
+							<img src="/selection.png" alt="Selection logo" class="mr-2 h-5/6 w-1/4" />Selection
+						</a>
+					</li>
+				</ul>
+				<div class="logout-div">
+					<!-- svelte-ignore a11y_click_events_have_key_events -->
+					<!-- svelte-ignore a11y_interactive_supports_focus -->
+					<a
+						id="logout-button"
+						class="logout-button mx-4 my-1 flex h-1/2 cursor-pointer items-center rounded bg-gray-700 p-2 text-white no-underline"
+						role="button"
+						data-sveltekit-reload
+						onclick={() => deconnexion()}
+					>
+						<img src="/logout.png" alt="Log-Out logo" class="mr-2 h-5/6 w-1/4" />Log-Out
+					</a>
+				</div>
+			</nav>
+		</div>
+	</div>
+	<div class="flex flex-grow flex-col items-center bg-gray-400">
+		<!-- rigth content-->
+		<div class="-mt-5 h-24 w-full">
+			<!-- top bar-->
+			<h1>Admin</h1>
+		</div>
+		<div class="mt-5 h-full w-full">
+			<!-- main content-->
+			{@render children()}
+		</div>
+		<!-- footer after -->
+	</div>
+</div>
