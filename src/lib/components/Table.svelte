5c90bbb05868a73729a20af6d02d1888b2484b09
Esther Paqui <paqui.esther@etu.iut-tlse3.fr>
2025-03-30 19:04:49 +0200

    Add Details and Table components with enhanced data handling and animations

--- src/lib/components/Table.svelte
+++ src/lib/components/Table.svelte
@@ -1,11 +1,12 @@
 <script>
 	import { onMount } from 'svelte';
+	import { fly } from 'svelte/transition';
 
 	let { data = null, dataSelected = $bindable() } = $props();
 
 	let previousIndex = $state(null);
 
-	let headers = $derived(data && data.length > 0 ? Object.keys(data[0]) : [])
+	let headers = $derived(data && data.length > 0 ? Object.keys(data[0]) : []);
 
 	const body = () => {
 		if (!data || data.length === 0) return [];
@@ -17,7 +18,6 @@
 
 		return res;
 	};
-
 
 	function isRowSelected(row, index) {
 		if (!dataSelected) return false;
@@ -38,11 +38,11 @@
 
 {#if data}
 	<div class="m-2">
-		<table class="table-auto border-2">
+		<table class="table-auto border-2" transition:fly={{ duration: 1000 }}>
 			<thead>
 				<tr>
 					{#each headers as header}
-						<th class="cursor-default bg-slate-300 border-2 p-2">
+						<th class="cursor-default border-2 bg-slate-300 p-2">
 							{header}
 						</th>
 					{/each}
@@ -64,5 +64,30 @@
 		</table>
 	</div>
 {:else}
-	<div>fetching</div>
+	<div>
+		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+			><path
+				fill="none"
+				stroke="currentColor"
+				stroke-dasharray="16"
+				stroke-dashoffset="16"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M12 3c4.97 0 9 4.03 9 9"
+				><animate
+					fill="freeze"
+					attributeName="stroke-dashoffset"
+					dur="0.2s"
+					values="16;0"
+				/><animateTransform
+					attributeName="transform"
+					dur="1.5s"
+					repeatCount="indefinite"
+					type="rotate"
+					values="0 12 12;360 12 12"
+				/></path
+			></svg
+		>
+	</div>
 {/if}
