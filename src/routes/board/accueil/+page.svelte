b1b83d2d08be9b6330a7e122754e0f1b5f9fd0fd
Esther Paqui <paqui.esther@etu.iut-tlse3.fr>
2025-04-03 10:34:15 +0100

    Add loading animations to the Accueil page for data fetching indicators

--- src/routes/board/accueil/+page.svelte
+++ src/routes/board/accueil/+page.svelte
@@ -131,11 +131,61 @@
 {#if data}
     <Card data={data} total={data.total} won={data.won} draw={data.draw} lost={data.lost} />
 {:else}
-    <div class="loading">Chargement des statistiques...</div>
+<div>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+        ><path
+            fill="none"
+            stroke="currentColor"
+            stroke-dasharray="16"
+            stroke-dashoffset="16"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 3c4.97 0 9 4.03 9 9"
+            ><animate
+                fill="freeze"
+                attributeName="stroke-dashoffset"
+                dur="0.2s"
+                values="16;0"
+            /><animateTransform
+                attributeName="transform"
+                dur="1.5s"
+                repeatCount="indefinite"
+                type="rotate"
+                values="0 12 12;360 12 12"
+            /></path
+        ></svg
+    >
+</div>
 {/if}
 {#if alldata}
     <Table data={alldata} />
 {:else}
-    <div class="loading">Chargement des données des joueurs...</div>
+<div>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+        ><path
+            fill="none"
+            stroke="currentColor"
+            stroke-dasharray="16"
+            stroke-dashoffset="16"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 3c4.97 0 9 4.03 9 9"
+            ><animate
+                fill="freeze"
+                attributeName="stroke-dashoffset"
+                dur="0.2s"
+                values="16;0"
+            /><animateTransform
+                attributeName="transform"
+                dur="1.5s"
+                repeatCount="indefinite"
+                type="rotate"
+                values="0 12 12;360 12 12"
+            /></path
+        ></svg
+    >
+</div>
 {/if}
 
